47d999b3d70680d8ee12f15b0a19ad598ae4e832
Marcin Krystianc <marcin.krystianc@gmail.com>
2021-08-31 22:19:15 +0200

    Show how to mitigate some memory leaks

---
+++ MSBuildIssue/MSBuildIssue/MyFs.cs
@@ -0,0 +1,84 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Build.Shared.FileSystem;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Microsoft.Build.FileSystem
+{
+    /// <summary>
+    /// Abstracts away some file system operations.
+    ///
+    /// Implementations:
+    /// - must be thread safe
+    /// - may cache some or all the calls.
+    /// </summary>
+    class MyFs : MSBuildFileSystemBase
+    {
+       
+        /// <summary>
+        /// Use this for var sr = new StreamReader(path)
+        /// </summary>
+        public override TextReader ReadFile(string path)
+        {
+            return new StreamReader(path);
+        }
+
+        public override Stream GetFileStream(string path, FileMode mode, FileAccess access, FileShare share)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ReadFileAllText(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override byte[] ReadFileAllBytes(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IEnumerable<string> EnumerateFiles(string path, string searchPattern, SearchOption searchOption)
+        {
+            return Directory.EnumerateFiles(path, searchPattern, searchOption);
+        }
+
+        public override IEnumerable<string> EnumerateDirectories(string path, string searchPattern, SearchOption searchOption)
+        {
+            return Directory.EnumerateDirectories(path, searchPattern, searchOption);
+        }
+
+        public override IEnumerable<string> EnumerateFileSystemEntries(string path, string searchPattern, SearchOption searchOption)
+        {
+            return Directory.EnumerateFileSystemEntries(path, searchPattern, searchOption);
+        }
+
+        public override FileAttributes GetAttributes(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override DateTime GetLastWriteTimeUtc(string path)
+        {
+            return File.GetLastWriteTimeUtc(path);
+        }
+
+        public override bool DirectoryExists(string path)
+        {
+            return Directory.Exists(path);
+        }
+
+        public override bool FileExists(string path)
+        {
+            return File.Exists(path);
+        }
+
+        public override bool FileOrDirectoryExists(string path)
+        {
+            return FileExists(path) || DirectoryExists(path);
+        }
+    }
+}
---
+++ MSBuildIssue/MSBuildIssue/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Microsoft.Build.Construction;
+using Microsoft.Build.Definition;
+using Microsoft.Build.Evaluation;
+using Microsoft.Build.Evaluation.Context;
+using Microsoft.Build.FileSystem;
+using Microsoft.Build.Locator;
+using CommandLine;
+
+namespace MSBuildIssue
+{
+    public class Options
+    {
+        [Option(Required = false, Default = true, HelpText = "")]
+        public bool? Manual { get; set; }
+
+        [Option(Required = false, Default = null, HelpText = "Pause after N iterations")]
+        public int? PauseAfterIterations { get; set; }
+
+        [Option(Required = false, Default = null, HelpText = "Path to the MSBuild")]
+        public string MSBuildPath { get; set; }
+
+        [Option(Required = true, Default = null, HelpText = "Path to the solution to be processed")]
+        public string SolutionPath { get; set; }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Environment.SetEnvironmentVariable("MSBUILDDISABLESDKCACHE", "1");
+            
+            Parser.Default.ParseArguments<Options>(args).WithParsed(options =>
+            {
+                if (options.MSBuildPath == null)
+                    MSBuildLocator.RegisterDefaults();
+                else
+                    MSBuildLocator.RegisterMSBuildPath(options.MSBuildPath);
+
+                var directoryName = Path.GetDirectoryName(options.SolutionPath);
+                var solutionFileName = Path.GetFileName(options.SolutionPath);
+                var originalDirectoryName = directoryName;
+                for (var i = 0;; i++)
+                {
+                    Console.WriteLine($"i:{i}");
+                    Console.WriteLine($"Press key to process solution");
+                    if (options.Manual.Value ||
+                        (options.PauseAfterIterations != null && i % options.PauseAfterIterations == 0 && i > 0))
+                        Console.ReadKey();
+                    Console.WriteLine($"Processing solution");
+                    var result = ProcessSolution(Path.Combine(directoryName, solutionFileName), options.Manual.Value);
+                    if (result == 0)
+                        throw new Exception("Unexpected result");
+
+                    var newDirectoryName = $"{originalDirectoryName}_{i}";
+                    Directory.Move(directoryName, newDirectoryName);
+                    directoryName = newDirectoryName;
+
+                    GC.Collect();
+                    GC.WaitForPendingFinalizers();
+                    GC.Collect();
+                }
+            });
+        }
+
+        static int ProcessSolution(string path, bool manual)
+        {
+            EvaluationContext.Create(EvaluationContext.SharingPolicy.Isolated);
+            var result = 0;
+            using (var projectCollection = new ProjectCollection())
+            {
+                var projectOptions = new ProjectOptions
+                {
+                    ProjectCollection = projectCollection,
+                    LoadSettings = ProjectLoadSettings.IgnoreEmptyImports | ProjectLoadSettings.IgnoreInvalidImports |
+                                   ProjectLoadSettings.RecordDuplicateButNotCircularImports | ProjectLoadSettings.IgnoreMissingImports,
+                    // IMPORTANT! - EvaluationContext.SharingPolicy.Shared - prevents from creating new evaluation context for each project (avoids memory leak)
+                    // IMPORTANT! - new MyFs() - custom FileSystem prevents from using CachingFileSystemWrapper (avoids memory leak)
+                    // IMPORTANT! - EvaluationContext is created for each project collection (it is not static) - this lets GC to release some memory when projects are unloaded
+                    EvaluationContext = EvaluationContext.Create(EvaluationContext.SharingPolicy.Shared, new MyFs()),
+                };
+                
+                var solutionFile = SolutionFile.Parse(path);
+                var projects = solutionFile.ProjectsInOrder
+                    .Where(x => x.ProjectType == SolutionProjectType.KnownToBeMSBuildFormat)
+                    .Select(x => Project.FromFile(x.AbsolutePath, projectOptions));
+            
+                foreach (var project in projects)
+                {
+                    result += project.Items.Count;
+                }
+
+                Console.WriteLine($"Press key to unload projects.");
+                if (manual) Console.ReadKey();
+                Console.WriteLine($"Unloading all projects.");
+                
+                projectCollection.UnloadAllProjects();
+            }
+
+            return result;
+        }
+    }
+}
